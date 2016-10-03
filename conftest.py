123ceb782e57c5fb4b11692b745d145ca8b6db8f
Olga Sentemova <osentemova@aligntech.com>
2016-10-03 14:11:55 +0300

    Add parametrizing;

--- conftest.py
+++ conftest.py
@@ -7,16 +7,20 @@ def basic_url():
     tree = etree.parse("config.xml")
     return tree.xpath("//url")[0].text
 
+def browsers():
+    tree = etree.parse("config.xml")
+    browsers_list = [node.text for node in tree.xpath("//browser")]
+    return browsers_list
 
 @pytest.fixture()
 def url_for_redirect():
     tree = etree.parse("config.xml")
     return tree.xpath("//redirected_url")[0].text
 
-@pytest.fixture(params=["Chrome", "Firefox"])
+@pytest.fixture(params=browsers())
 def browser_handler(request):
     browser_type = request.param
-    driver = getattr(webdriver,browser_type)()
+    driver = getattr(webdriver, browser_type)()
     driver.implicitly_wait(10)
     driver.get(basic_url())
     def browser_teardown():
